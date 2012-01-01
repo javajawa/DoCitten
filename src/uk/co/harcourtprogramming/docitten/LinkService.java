751eeac995965c91688ec0432ef42bcf5e28ac2e
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-01-01 00:59:21 +0000

    Implement the LinkService

--- src/uk/co/harcourtprogramming/docitten/LinkService.java
+++ src/uk/co/harcourtprogramming/docitten/LinkService.java
@@ -1,7 +1,18 @@
 package uk.co.harcourtprogramming.docitten;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import uk.co.harcourtprogramming.internetrelaycats.MessageService;
@@ -21,7 +32,188 @@ public class LinkService extends Service implements MessageService
 	 * matched.
 	 */
 	private final static Pattern uriPattern =
-		Pattern.compile("(https?://)?(\\w+\\.)+(com|net|uk|edu|is.gd|bit.ly)(/[\\w.#?%=+-~]*)*", Pattern.CASE_INSENSITIVE);
+		Pattern.compile("(https?://)?(\\w+\\.)*(www|com|net|uk|edu|is.gd|bit.ly)(/[^\\s]*)*", Pattern.CASE_INSENSITIVE);
+	private final static Pattern protocolPattern =
+		Pattern.compile("https?://.+", Pattern.CASE_INSENSITIVE);
+
+	/**
+	 * The logger for the Link Service (
+	 */
+	private final static Logger log = Logger.getLogger("docitten.LinkService");
+	/**
+	 * <p>Thread group for running link resolvers in</p.
+	 * <p>The group is marked as a Daemon group, and so will be ignored by the
+	 * JVM when it comes to determining the nubmer of important running threads</p>
+	 */
+	private final static ThreadGroup THREAD_GROUP = new ThreadGroup("LinkResolvers") {
+		@Override
+		public void uncaughtException(Thread t, Throwable e)
+		{
+			log.log(Level.SEVERE, "Excpetion in " + t.getName(), e);
+		}
+	};
+
+	static
+	{
+		Handler h = new ConsoleHandler();
+		h.setFormatter(new Formatter()
+		{
+			@Override
+			public String format(LogRecord l)
+			{
+				Calendar time = Calendar.getInstance();
+				time.setTimeInMillis(l.getMillis());
+
+				String mess = String.format("[%2$tR %1$s] %3$s\n",
+					l.getLevel().getLocalizedName(), time, formatMessage(l));
+				if (l.getThrown() != null)
+				{
+					Throwable t =  l.getThrown();
+					mess += t.getMessage() + '\n';
+					for (StackTraceElement ste : t.getStackTrace())
+					{
+						mess += '\t' + ste.toString() + '\n';
+					}
+				}
+				return mess;
+			}
+		});
+		log.addHandler(h);
+		log.setUseParentHandlers(false);
+		THREAD_GROUP.setDaemon(false);
+	}
+
+	private static final String pre = "kMGTPE";
+	private static String humanReadableByteCount(long bytes) {
+		if (bytes < 1024) return bytes + " B";
+		int exp = (int) (Math.log(bytes) / Math.log(1024));
+		return String.format("%.1f %siB", bytes / Math.pow(1024, exp), pre.charAt(exp-1));
+	}
+
+	private class LinkResolver implements Runnable
+	{
+		private final URL baseURI;
+		private final Message mess;
+
+		private LinkResolver(String baseURI, Message mess) throws MalformedURLException
+		{
+			if (!protocolPattern.matcher(baseURI).matches())
+			{
+				this.baseURI = new URL("http://" + baseURI);
+			}
+			else
+			{
+				this.baseURI = new URL(baseURI);
+			}
+			this.mess = mess;
+		}
+
+		@Override
+		public void run()
+		{
+			try
+			{
+				URL curr = baseURI;
+				HttpURLConnection conn;
+				boolean resolved = false;
+
+				while (true)
+				{
+					conn = (HttpURLConnection)curr.openConnection();
+					conn.setInstanceFollowRedirects(false);
+					conn.setRequestMethod("HEAD");
+					conn.connect();
+
+					switch (conn.getResponseCode())
+					{
+						case HttpURLConnection.HTTP_ACCEPTED:
+						case HttpURLConnection.HTTP_CREATED:
+						case HttpURLConnection.HTTP_NO_CONTENT:
+						case HttpURLConnection.HTTP_OK:
+						case HttpURLConnection.HTTP_PARTIAL:
+						case HttpURLConnection.HTTP_RESET:
+						case HttpURLConnection.HTTP_NOT_MODIFIED:
+							resolved = true;
+							break;
+
+						case HttpURLConnection.HTTP_BAD_GATEWAY:
+						case HttpURLConnection.HTTP_BAD_METHOD:
+						case HttpURLConnection.HTTP_BAD_REQUEST:
+						case HttpURLConnection.HTTP_CLIENT_TIMEOUT:
+						case HttpURLConnection.HTTP_CONFLICT:
+						case HttpURLConnection.HTTP_ENTITY_TOO_LARGE:
+						case HttpURLConnection.HTTP_FORBIDDEN:
+						case HttpURLConnection.HTTP_GATEWAY_TIMEOUT:
+						case HttpURLConnection.HTTP_GONE:
+						case HttpURLConnection.HTTP_INTERNAL_ERROR:
+						case HttpURLConnection.HTTP_LENGTH_REQUIRED:
+						case HttpURLConnection.HTTP_NOT_ACCEPTABLE:
+						case HttpURLConnection.HTTP_NOT_AUTHORITATIVE:
+						case HttpURLConnection.HTTP_NOT_FOUND:
+						case HttpURLConnection.HTTP_NOT_IMPLEMENTED:
+						case HttpURLConnection.HTTP_PAYMENT_REQUIRED:
+						case HttpURLConnection.HTTP_PRECON_FAILED:
+						case HttpURLConnection.HTTP_PROXY_AUTH:
+						case HttpURLConnection.HTTP_REQ_TOO_LONG:
+						case HttpURLConnection.HTTP_UNAUTHORIZED:
+						case HttpURLConnection.HTTP_UNAVAILABLE:
+						case HttpURLConnection.HTTP_UNSUPPORTED_TYPE:
+						case HttpURLConnection.HTTP_USE_PROXY:
+						case HttpURLConnection.HTTP_VERSION:
+							return;
+
+						case HttpURLConnection.HTTP_MOVED_PERM:
+						case HttpURLConnection.HTTP_MOVED_TEMP:
+						case HttpURLConnection.HTTP_MULT_CHOICE:
+						case HttpURLConnection.HTTP_SEE_OTHER:
+							if (conn.getHeaderField("Location") == null) return;
+							curr = URI.create(curr.toExternalForm()).resolve(
+								conn.getHeaderField("location")).toURL();
+							break;
+
+						default:
+							log.log(Level.WARNING, "Unknown HTTP Status code returned: {0}", conn.getResponseCode());
+							return;
+					}
+					conn.disconnect();
+					if (resolved) break;
+				}
+
+				conn = (HttpURLConnection)curr.openConnection();
+				conn.setRequestMethod("GET");
+				conn.connect();
+
+				String mime = conn.getContentType();
+				if (mime == null) mime = "";
+				mime = mime.split(";")[0];
+
+				if (conn.getContentType().matches("(text/.+|.+xhtml.+)")) // A HTML/XHTML file (most likely)
+				{
+					mess.replyToAll(String.format("[%s] %s", curr.getHost(), "<TODO: Retreive Title>"));
+				}
+				else
+				{
+					if (conn.getContentLength() == -1)
+					{
+						mess.replyToAll(String.format("[%s] %s (size unknown)", curr.getHost(), mime));
+					}
+					else
+					{
+						mess.replyToAll(String.format("[%s] %s %s",
+							curr.getHost(),
+							mime,
+							humanReadableByteCount(conn.getContentLength())
+						));
+					}
+				}
+			}
+			catch (Throwable ex)
+			{
+				throw new RuntimeException(ex);
+			}
+		}
+
+	}
 
 	/**
 	 * Create a link server
@@ -55,7 +247,7 @@ public class LinkService extends Service implements MessageService
 			System.out.println("" + uris(l).size() + '\t' + l);
 		}
 
-		List<String> s = uris("Hello there hello.com and example.com/?q=bob are spam sites");
+		Set<String> s = uris("Hello there hello.com and example.com/?q=bob are spam sites");
 		System.out.println("Hello there hello.com and example.com/?q=bob are spam sites");
 		for (String t : s)
 		{
@@ -66,9 +258,22 @@ public class LinkService extends Service implements MessageService
 	@Override
 	public void handle(Message m)
 	{
-		List<String> uris = uris(m.getMessage());
+		Set<String> uris = uris(m.getMessage());
 		for (String uri : uris)
-			m.replyToAll("Link Detected: " + uri);
+			try
+			{
+				LinkResolver l = new LinkResolver(uri, m);
+				if (THREAD_GROUP.isDestroyed())
+					throw new IllegalThreadStateException("Thread Group destoryed whilst trying to start search for " + uri);
+				Thread t = new Thread(THREAD_GROUP, l, "Link Resolver: " + uri);
+				t.setDaemon(true);
+				t.start();
+			}
+			catch (MalformedURLException ex)
+			{
+				Logger.getLogger(LinkService.class.getName()).
+					log(Level.SEVERE, "Malformed URI: " + uri, ex);
+			}
 	}
 
 	/**
@@ -76,22 +281,24 @@ public class LinkService extends Service implements MessageService
 	 * @param message the message to scan for links
 	 * @return the list of links that are found (may by empty, but not null)
 	 */
-	public static List<String> uris(String message)
+	public static Set<String> uris(String message)
 	{
-		Matcher m = uriPattern.matcher(message);
-		final List<String> r = new ArrayList<String>();
+		final Set<String> r = new HashSet<String>();
 
+		Matcher m = uriPattern.matcher(message);
 		while (m.find())
-		{
 			r.add(m.group());
-		}
+
+		m = protocolPattern.matcher(message);
+		while (m.find())
+			r.add(m.group());
+
 		return r;
 	}
 
 	@Override
 	public void shutdown()
 	{
-		// Nothing to see here. Move along, citizen!
+		// TODO: clean up threads
 	}
 }
-
