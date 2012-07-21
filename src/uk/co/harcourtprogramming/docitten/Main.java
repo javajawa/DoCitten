25093ff29ba964a635139bf3962cc983d0b7ceab
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-07-21 12:49:52 +0100

    Srot imports in Main class

--- src/uk/co/harcourtprogramming/docitten/Main.java
+++ src/uk/co/harcourtprogramming/docitten/Main.java
@@ -1,11 +1,11 @@
 package uk.co.harcourtprogramming.docitten;
 
-import static java.util.Arrays.asList;
-import static java.util.Arrays.copyOfRange;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
+import static java.util.Arrays.asList;
+import static java.util.Arrays.copyOfRange;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
