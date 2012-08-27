5ee34231d387ed7a8791dbdba52c5f700464e57b
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-08-27 12:48:35 +0100

    KittenService: Clean up and JavaDocs
    Clean up the KittenService using the updated hints I have available

--- src/uk/co/harcourtprogramming/docitten/KittenService.java
+++ src/uk/co/harcourtprogramming/docitten/KittenService.java
@@ -1,18 +1,23 @@
 package uk.co.harcourtprogramming.docitten;
 
 import java.util.List;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 import java.util.Random;
-import uk.co.harcourtprogramming.internetrelaycats.MessageService;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import uk.co.harcourtprogramming.internetrelaycats.Message;
+import uk.co.harcourtprogramming.internetrelaycats.MessageService;
 import uk.co.harcourtprogramming.internetrelaycats.RelayCat;
 import uk.co.harcourtprogramming.internetrelaycats.Service;
 
+/**
+ * <p>Service that emulates some kitten-like behaviour in order to keep channels
+ * relaxed and happy.</p>
+ * @author Benedict Harcourt / javajawa
+ */
 public class KittenService extends Service implements MessageService
 {
 	/**
-	 * Words that resolve to the concept of 'kitten'
+	 * <p>Words that resolve to the concept of 'kitten'</p>
 	 */
 	private static final Pattern kitten =
 		Pattern.compile(
@@ -20,7 +25,7 @@ public class KittenService extends Service implements MessageService
 			Pattern.CASE_INSENSITIVE
 		);
 	/**
-	 * Phrases that resolve to the sounds that cats make
+	 * <p>Phrases that resolve to the sounds that cats make</p>
 	 */
 	private static final Pattern mewls =
 		Pattern.compile(
@@ -28,27 +33,32 @@ public class KittenService extends Service implements MessageService
 			Pattern.CASE_INSENSITIVE
 		);
 	/**
-	 * Phrases that resolve to things that kitten like being done to them
+	 * <p>Phrases that resolve to things that kitten like being done to them</p>
 	 */
 	private static final Pattern attention =
 		Pattern.compile(
-			"(?:^|\\s)(scritchl?es|pets|cud+les|paws(?: at)?|hugs|feeds|greets|nuz+les|(?:dangles|gives).+(string|yarn|wool|catnip))",
+			"(?:^|\\s)(s[ck]ritchl?es|strokes|pets|cud+les|paws(?: at)?|hugs|feeds|greets|nuz+les|(?:dangles|gives).+(?:string|yarn|wool|catnip))",
 			Pattern.CASE_INSENSITIVE
 		);
 
 	/**
-	 * Random number source for randomised responses
+	 * <p>Entropy source for randomised responses</p>
 	 */
 	private final Random r;
 
 	/**
-	 * Creates the kitten service
+	 * <p>Creates an instance the kitten service</p>
 	 */
 	public KittenService()
 	{
 		r = new Random();
 	}
 
+	/**
+	 * <p>Creates an instance of the kitten service using a specified entropy
+	 * source.</p>
+	 * @param r Entropy source for this kitten
+	 */
 	public KittenService(Random r)
 	{
 		this.r = r;
@@ -59,55 +69,61 @@ public class KittenService extends Service implements MessageService
 	{
 		final String mess = m.getMessage();
 
-		if (m.isAction())
+		if (m.isAction() && mess.toLowerCase().contains(m.getNick().toLowerCase()))
 		{
-			if (mess.toLowerCase().contains(m.getNick().toLowerCase()))
-			{
-				Matcher attentionMatcher = attention.matcher(mess);
-				if (attentionMatcher.find())
-				{
-					m.act(attend(m.getSender()));
-				}
-			}
+			Matcher attentionMatcher = attention.matcher(mess);
+			if (attentionMatcher.find())
+				m.act(attend(m.getSender()));
 		}
 
 		// Stop DoCitten replying to itself as much
 		if (mess.equals("mew =^.^=")) return;
 
-		String reply = "";
+		StringBuilder reply = new StringBuilder(100);
 
+		// Find things we need to mewl at
 		Matcher kittenMatcher = kitten.matcher(mess);
-		Matcher mewlsMatcher  = mewls .matcher(mess);
+		while (kittenMatcher.find())
+			reply.append(mewl()).append(' ');
 
-		// Note that mewl() adds a trailing space
-		while (kittenMatcher.find()) reply += mewl();
-		while (mewlsMatcher.find())  reply += mewl();
+		Matcher mewlsMatcher  = mewls .matcher(mess);
+		while (mewlsMatcher.find())
+			reply.append(mewl()).append(' ');
 
 		if (reply.length() != 0)
 		{
-			m.replyToAll(reply + "=^.^=");
+			reply.append("=^.^=");
+			m.replyToAll(reply.toString());
 		}
 	}
 
 	/**
-	 * @return a random(ish) mewling sound with trailing space
+	 * <p>Generates a 'mewl' based on the entropy source of this kitten</p>
+	 * @return a random(ish) mewling sound
 	 */
 	private String mewl()
 	{
 		switch (r.nextInt(6))
 		{
-			case 0: return "nyaann ";
-			case 1: return "mraow ";
-			default: return "mew ";
+			case 0: return "nyaann";
+			case 1: return "mraow";
+			default: return "mew";
 		}
 	}
 
 	/**
-	 * @param user the user attention is given to
+	 * <p>Get a string to be used as an action which indicates that the kitten
+	 * is paying attention to something that just happened.</p>
+	 * <p>The kitten may choose to attend in general, or pay attention to a given
+	 * specific user.</p>
+	 * @param user the user attention is given to. If null, "nice people" is
+	 * substituted.
 	 * @return how the kitten responds to the user, or null if it ignores the user
 	 */
 	private String attend(String user)
 	{
+		if (user == null)
+			user = "nice people";
 		switch (r.nextInt(9))
 		{
 			case 0: return "purrs";
@@ -130,6 +146,7 @@ public class KittenService extends Service implements MessageService
 		{
 			HelpService.HelpInfo help = new HelpService.HelpInfo("Kitten Service", "Kittens do not need your help. They do appreciate being looked after. But, there are plenty of other expendable humans to do that...");
 			helpServices.get(0).addHelp("kitten", help);
+			helpServices.get(0).addHelp("kittens", help);
 		}
 	}
 
