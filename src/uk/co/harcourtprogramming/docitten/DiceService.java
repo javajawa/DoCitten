ab0cb53a05077fa6eeb33e7c2d943e897d021223
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-11-02 18:22:34 +0000

    Documentation for DiceService

--- src/uk/co/harcourtprogramming/docitten/DiceService.java
+++ src/uk/co/harcourtprogramming/docitten/DiceService.java
@@ -1,11 +1,8 @@
 package uk.co.harcourtprogramming.docitten;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import uk.co.harcourtprogramming.internetrelaycats.Message;
@@ -15,13 +12,21 @@ import uk.co.harcourtprogramming.internetrelaycats.Service;
 import uk.co.harcourtprogramming.mewler.MessageTokeniser;
 
 /**
+ * <p>Service for generating dice rolls for users</p>
  *
- * @author Ben
+ * @author Benedict Harcourt / javajawa
  */
 public class DiceService extends Service implements MessageService
 {
+	/**
+	 * <p>Random number generator for dice rolls
+	 */
 	private final Random r = new Random();
-	private static Pattern dicePattern = Pattern.compile("roll(?<mode> (?:sum|product|base))?(?<dice>(?: [0-9]*d[0-9]+d?)+)", Pattern.CASE_INSENSITIVE);
+	/**
+	 * <p>Pattern which matches all of the dice commands that this service
+	 * recognises</p>
+	 */
+	private final static Pattern dicePattern = Pattern.compile("roll(?<mode> (?:sum|product|base))?(?<dice>(?: [0-9]*d[0-9]+d?)+)", Pattern.CASE_INSENSITIVE);
 
 	@Override
 	protected void shutdown()
@@ -32,7 +37,15 @@ public class DiceService extends Service implements MessageService
 	@Override
 	protected void startup(RelayCat r)
 	{
-		// Nothing to see here. Move along, citizen!
+		List<HelpService> helpServices = r.getServicesByClass(HelpService.class);
+
+		if (!helpServices.isEmpty())
+		{
+			HelpService.HelpInfo help = new HelpService.HelpInfo("Dice Service", "DoCitten can be asked to roll dice\r\nDoCitten: roll [mode] <dice>[ <dice> ...]\r\nYou can try 'roll d6', or look at the subtopics for information .");
+			help.addChild("mode", new HelpService.HelpInfo("Dice Modes", "The Dice service can combine in one of three ways\r\n \r\nsum:     Dice values are added\r\nproduct: Dice are multipled together\r\nbase:    Dice are treated each as a digit in base n, when n is the number of sides on the dice."));
+			help.addChild("dice", new HelpService.HelpInfo("Dice Specification", "Dice are speificed in a simpe syntax: [number]d<sides>[d]\r\nFor example, you would ask for a six-sided dice to be rolled four times as '4d6'.\r\nThe final d converts the die to a doubling die."));
+			helpServices.get(0).addHelp("dice", help);
+		}
 	}
 
 	@Override
@@ -69,21 +82,40 @@ public class DiceService extends Service implements MessageService
 				}
 			}
 
-			switch (mode)
+			if (dice.length > 1)
 			{
-				case SUM: case PRODUCT: s.append("Total: ").append(total);
+				switch (mode)
+				{
+					case SUM: case PRODUCT: s.append("Total: ").append(total);
+				}
 			}
 
 			m.replyToAll(s.toString());
 		}
 	}
 
+	/**
+	 * <p>A class that represents a matched group of similar dice.</p>
+	 */
 	private final class Die
 	{
-		protected final int count;
-		protected final int sides;
-		protected final boolean doubling;
+		/**
+		 * <p>The number of times this dice is to be rolled</p>
+		 */
+		final int count;
+		/**
+		 * <p>The number of sides (faces) each die has</p>
+		 */
+		final int sides;
+		/**
+		 * <p>Whether the sides are numbered carindally, or as powers of two</p>
+		 */
+		final boolean doubling;
 
+		/**
+		 * <p>Creates a new Die object based on a specification string</p>
+		 * @param spec A string of the form [0-9]*d[0-9]+d?
+		 */
 		private Die(String spec)
 		{
 			MessageTokeniser t = new MessageTokeniser(spec);
@@ -103,6 +135,12 @@ public class DiceService extends Service implements MessageService
 			this.sides = Integer.parseInt(c);
 		}
 
+		/**
+		 * <p>Rolls these dice</p>
+		 * @param mode how the individual rolls are combined
+		 * @param rolls an array of ints, at least as long as {@link #count}
+		 * @return the total value of the rolls
+		 */
 		private int roll(DiceMode mode, int[] rolls)
 		{
 			int result = 0;
@@ -148,12 +186,30 @@ public class DiceService extends Service implements MessageService
 		}
 	}
 
-	protected enum DiceMode
+	/**
+	 * <p>Different modes for combining individual die rolls</p>
+	 */
+	private enum DiceMode
 	{
+		/**
+		 * <p>Dice values are added together</p>
+		 */
 		SUM,
+		/**
+		 * <p>Dice values are multiplied together</p>
+		 */
 		PRODUCT,
+		/**
+		 * <p>Dice values are treated as a string in base n, where n is the
+		 * number of sides on the dice</p>
+		 */
 		BASE;
 
+		/**
+		 * <p>Converts a string name of a DiceMode to the DiceMode value</p>
+		 * @param s The String to convert
+		 * @return The mode, or SUM if no match was found
+		 */
 		public static DiceMode parse(String s)
 		{
 			if (s == null)
