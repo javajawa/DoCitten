f2d0584861b0cd39c01c0198389394b797cc3fe5
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2016-03-26 12:26:45 +0000

    Add Giphy tests to LinkFinderTest
    Adds a number of test cases for finding Giphy requests in user input,
    and checks that the returned search terms are correct.

--- test/uk/co/harcourtprogramming/docitten/LinkFinderTest.java
+++ test/uk/co/harcourtprogramming/docitten/LinkFinderTest.java
@@ -70,7 +70,17 @@ public class LinkFinderTest
 			new Object[]{new LinkData("  github.com", "github.com")},
 			new Object[]{new LinkData("[github.com]", "github.com")},
 			new Object[]{new LinkData("example.com:8080", "example.com:8080")},
-			new Object[]{new LinkData("http://www.google.com", "http://www.google.com")}
+			new Object[]{new LinkData("http://www.google.com", "http://www.google.com")},
+			new Object[]{new LinkData("gif:cat", "cat")},
+			new Object[]{new LinkData("giphy:cat", "cat")},
+			new Object[]{new LinkData("gif:\"blood sword\"", "blood sword")},
+			new Object[]{new LinkData("gif:\"blood\" \"sword\"", "blood")},
+			new Object[]{new LinkData("gif:???")},
+			new Object[]{new LinkData("gif:\"???\"", "???")},
+			new Object[]{new LinkData("gif:google.com", "google")}, // This is a bit of a weird one...
+			new Object[]{new LinkData("google.com www", "google.com", "www")},
+			new Object[]{new LinkData("gif: cat")},
+			new Object[]{new LinkData("gif:cat giphy:dog", "cat", "dog")}
 		});
 	}
 	/**
